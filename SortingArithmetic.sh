fc847b188948d74a263a75f551a08c5303e24b13
Shahid-sk <mail2sk.shahid@gmail.com>
2021-06-13 12:46:49 +0530

    o/p of exp2

--- SortingArithmetic.sh
+++ SortingArithmetic.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -x
 echo "welcome to sorting Arithmetic problem"
+part_time_hours=8
 read -p "enter a value:" a
 read -p "enter b value:" b
 read -p "enter c value:" c
 exp1=$(($a+$b*$c))
 echo $exp1
+exp2=$(($a*$b+$c))
+echo $exp2
