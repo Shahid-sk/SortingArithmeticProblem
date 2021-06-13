20adaae813820f3b535727ab9dc849cc8cae3df7
Shahid-sk <mail2sk.shahid@gmail.com>
2021-06-13 14:35:46 +0530

    added into dictionary

--- SortingArithmetic.sh
+++ SortingArithmetic.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 echo "welcome to sorting Arithmetic problem"
 part_time_hours=8
+declare -A Sorting
 read -p "enter a value:" a
 read -p "enter b value:" b
 read -p "enter c value:" c
@@ -12,3 +13,8 @@ exp3=$(($c+$a/$b))
 echo $exp3
 exp4=$(($a%$b+$c))
 echo $exp4
+
+Sorting[exp1]="$exp1"
+Sorting[exp2]="$exp2"
+Sorting[exp3]="$exp3"
+Sorting[exp4]="$exp4"
