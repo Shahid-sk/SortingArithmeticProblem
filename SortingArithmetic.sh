98de50146eab4dfced8fcdb2c5a178115c4dfc7c
Shahid-sk <mail2sk.shahid@gmail.com>
2021-06-14 15:45:32 +0530

    data retrived from dictionary to array

--- SortingArithmetic.sh
+++ SortingArithmetic.sh
@@ -2,6 +2,7 @@
 echo "welcome to sorting Arithmetic problem"
 part_time_hours=8
 declare -A Sorting
+declare -a Sorting2
 read -p "enter a value:" a
 read -p "enter b value:" b
 read -p "enter c value:" c
@@ -17,4 +18,19 @@ Sorting[exp1]="$exp1"
 Sorting[exp2]="$exp2"
 Sorting[exp3]="$exp3"
 Sorting[exp4]="$exp4"
+
+
+counter=0
+for (( i=1; i<=${#Sorting[@]}; i++ ))
+do
+Sorting2[((counter++))]=${Sorting[exp$i]}
+done
+echo ${Sorting2[@]}
+
+
+
+for index in ${!Sorting[@]}
+do
+        echo $index=${Sorting[$index]}
+done
 
